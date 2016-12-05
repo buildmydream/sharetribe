385ea3f03a22d3286e039dc4f4416facfc3c32e3
Kimmo Puputti <kimmo@sharetribe.com>
2016-12-05 16:28:01 +0200

    wip: availability actions and reducer placeholders

--- client/app/reducers/ManageAvailabilityReducer.js
+++ client/app/reducers/ManageAvailabilityReducer.js
@@ -1,5 +1,30 @@
-const initialState = {};
+import * as actionTypes from '../constants/ManageAvailabilityConstants';
 
-export default function manageAvailabilityReducer(state = initialState) {
-  return state;
+const initialState = {
+
+};
+
+export default function manageAvailabilityReducer(state = initialState, action) {
+  const { type } = action;
+
+  switch (type) {
+    case actionTypes.ALLOW_DAY:
+      // TODO: mark day as allowed in the pending list
+      return state;
+    case actionTypes.BLOCK_DAY:
+      // TODO: mark day as blocked in the pending list
+      return state;
+    case actionTypes.CHANGE_MONTH:
+      // TODO: fetch more data
+      return state;
+    case actionTypes.SAVE_CHANGES:
+      // TODO: save pending changes
+      return state;
+    case actionTypes.CLOSE_EDIT_VIEW:
+      // TODO: confirm if there are unsaved changes
+      // TODO: mark winder closed
+      return state;
+    default:
+      return state;
+  }
 }
